481e044ee28ee162cced870242257bc70c855c77
Femilove <femilove57@gmail.com>
2025-03-16 20:59:29 +0100

    new

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:10000';
+const BASE_URL = 'https://investplus-backend.onrender.com/';
 
 export default axios.create({
     baseURL: BASE_URL
